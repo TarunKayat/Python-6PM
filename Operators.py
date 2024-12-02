0174b3ab2f3b2d509cf15dea313b4d92e7f8ae0e
Mrk <123792284+Mrk-ctrl@users.noreply.github.com>
2024-12-02 18:50:58 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,74 @@
+a = 20
+b = 10
+
+# Arithamtic Operators
+# +
+# -
+# *
+# **
+# / - Gives Quotient in float
+#! // - Gives Quotient in int also called floor division
+# % - Gives Remainder
+
+print("Arithamtic Operators")
+# print("The Addition of", a, "and", b, "is", a + b)
+print(f"The Addition of {a} and {b} is: {a + b}")
+print(f"The Subtraction of {a} and {b} is: {a - b}")
+print(f"The Multiplication of {a} and {b} is: {a * b}")
+print(f"The Power of 2 to 3 is: {2 ** 3}")
+print(f"The Division of {a} and {b} is: {a / b}")
+print(f"The Floor Division of {a} and {b} is: {a // b}")
+print(f"The Remainder of {a} and {b} is: {a % b}")
+print("=========================")
+
+# Assingment Operators
+# =
+# +=
+# -=
+# *=
+# **=
+# /=
+#! //=
+# %=
+
+# a += 10 # 30
+# print(f"The value of a is: {a}")
+
+# a -= 10 # 20
+# print(f"The value of a is: {a}")
+
+# a *= 10 # 200
+# print(f"The value of a is: {a}")
+
+# a //= 10 # 20
+# print(f"The value of a is: {a}")
+
+# a %= 10 # 0
+# print(f"The value of a is: {a}")
+
+# Relational Operators - Comparison Operators
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+
+print("Relational Operators - Comparison Operators")
+print(f"The answer is: {a == b}")
+print(f"The answer is: {a != b}")
+print(f"The answer is: {a < b}")
+print(f"The answer is: {a > b}")
+print(f"The answer is: {a <= b}")
+print(f"The answer is: {a >= b}")
+print("=========================")
+
+# Logical Operators
+# and
+# or
+# not
+
+print("Logical Operators")
+print(f"The answer is: {a > 10 and b < 5}")
+print(f"The answer is: {a > 10 or b < 5}")
+print(f"The answer is: {not a == b}")
